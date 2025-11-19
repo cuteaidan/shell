4f3a63421d6dd61e60f864b345d5df9d4c2abf59
cuteaidan <cuteaidan@qq.com>
2025-11-18 18:41:46 -0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -23,8 +23,8 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # ====== 配置部分 ======
-CONFIG_URL="${CONFIG_URL:-https://raw.githubusercontent.com/cuteaidan/shell/refs/heads/main/scripts.conf}"
-BACKUP_URL="https://raw.eaidan.com/cuteaidan/shell/refs/heads/main/scripts.conf"
+CONFIG_URL="${CONFIG_URL:-https://raw.eaidan.com/cuteaidan/shell/refs/heads/main/scripts.conf}"
+BACKUP_URL="https://raw.githubusercontent.com/cuteaidan/shell/refs/heads/main/scripts.conf"
 PER_PAGE=10
 BOX_WIDTH=41
 LEFT_INDENT="        "
